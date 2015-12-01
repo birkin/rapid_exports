b94797d6e5af12be8cd1c50f48c1c265ab1399d4
birkin <birkin.diana@gmail.com>
2015-12-01 11:43:24 -0500

    redirects root url.

--- rapid_app/urls_app.py
+++ rapid_app/urls_app.py
@@ -7,7 +7,7 @@ from django.views.generic import RedirectView
 
 urlpatterns = patterns('',
 
-    url( r'^hi/$',  'rapid_app.views.hi', name='hi_url' ),
+    url( r'^info/$',  'rapid_app.views.hi', name='info_url' ),
 
     url( r'^$',  RedirectView.as_view(pattern_name='info_url') ),
 
