30fe2b73a3867a42513ac239507cd8bf71ed2e83
birkin <birkin.diana@gmail.com>
2015-12-17 08:55:34 -0500

    adds facets to admin dev-db preview.

--- rapid_app/admin.py
+++ rapid_app/admin.py
@@ -12,6 +12,7 @@ class PrintTitleDevAdmin( admin.ModelAdmin ):
         'key', 'issn', 'start', 'end', 'location', 'call_number', 'date_updated' ]
     search_fields = list_display
     readonly_fields = list_display
+    list_filter = [ 'location', 'issn' ]
 
     # end class
 
