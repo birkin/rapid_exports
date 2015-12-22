6f611dc290f13d74fe6706ad6d40845765a4ee16
birkin <birkin.diana@gmail.com>
2015-12-22 09:04:56 -0500

    adds building to admin display.

--- rapid_app/admin.py
+++ rapid_app/admin.py
@@ -9,7 +9,7 @@ class PrintTitleDevAdmin( admin.ModelAdmin ):
     ordering = [ 'key' ]
     date_hierarchy = 'date_updated'
     list_display = [
-        'key', 'issn', 'start', 'end', 'location', 'call_number', 'date_updated' ]
+        'key', 'issn', 'start', 'end', 'call_number', 'building', 'location', 'date_updated' ]
     search_fields = list_display
     # readonly_fields = list_display
     list_filter = [ 'location', 'issn' ]
