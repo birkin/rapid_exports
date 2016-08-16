b0a03b828d9cf931025b7b7a59e856cda8ff108e
birkin <birkin.diana@gmail.com>
2016-08-16 14:14:42 -0400

    simplifies urls.

--- config/urls.py
+++ config/urls.py
@@ -2,14 +2,32 @@
 
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-
+from django.views.generic import RedirectView
 
 admin.autodiscover()
+
 
 urlpatterns = patterns('',
 
     url( r'^admin/', include(admin.site.urls) ),
 
-    url( r'^', include('rapid_app.urls_app') ),
+    url( r'^info/$',  'rapid_app.views.hi', name='info_url' ),
 
-)
+    url( r'^tasks/$',  'rapid_app.views.tasks', name='tasks_url' ),
+
+    url( r'^tasks/process_file_from_rapid/$',  'rapid_app.views.process_file_from_rapid', name='process_file_from_rapid_url' ),
+
+    url( r'^tasks/update_titles_table/$',  'rapid_app.views.update_titles', name='update_titles_url' ),
+
+    url( r'^$',  RedirectView.as_view(pattern_name='info_url') ),
+
+    )
+
+
+# urlpatterns = patterns('',
+
+#     url( r'^admin/', include(admin.site.urls) ),
+
+#     url( r'^', include('rapid_app.urls_app') ),
+
+# )
