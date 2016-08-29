4a5ecd7cf7792c8cc5c5834fea98930c0be5ec03
birkin <birkin.diana@gmail.com>
2016-08-29 10:11:04 -0400

    removes commented-out code.

--- rapid_app/models.py
+++ rapid_app/models.py
@@ -64,32 +64,6 @@ class ProcessorTracker( models.Model ):
 #########################
 
 
-# class ProcessFileFromRapidHelper( object ):
-#     """ Manages views.process_file_from_rapid() work. """
-
-#     def initiate_work( self, request ):
-#         """ Initiates work.
-#             Called by views.process_file_from_rapid() """
-#         log.debug( 'source-path, ```{source}```; utf8-destination-path, ```{destination}```'.format(source=settings_app.FROM_RAPID_FILEPATH, destination=settings_app.FROM_RAPID_UTF8_FILEPATH) )
-#         processor = RapidFileProcessor(
-#             settings_app.FROM_RAPID_FILEPATH, settings_app.FROM_RAPID_UTF8_FILEPATH )
-#         data_lst = processor.parse_file_from_rapid()
-#         return data_lst
-
-#     def make_response( self, request, data ):
-#         """ Prepares response.
-#             Called by views.process_file_from_rapid() """
-#         if request.GET.get( 'format' ) == 'json':
-#             output = json.dumps( data, sort_keys=True, indent=2 )
-#             resp = HttpResponse( output, content_type=u'application/javascript; charset=utf-8' )
-#         else:
-#             # resp = HttpResponseRedirect( reverse('tasks_url') )
-#             resp = HttpResponseRedirect( '/rapid_manager/admin/rapid_app/printtitledev/' )
-#         return resp
-
-#     # end class ProcessFileFromRapidHelper
-
-
 class UpdateTitlesHelper( object ):
     """ Manages views.update_production_easyA_titles() work. """
 
